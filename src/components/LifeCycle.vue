3072ad41ded671a37fade0a2ed78b02084e5a3c8
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-01-31 21:23:45 -0300

    primeiro commit

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+!
+<template>
+  <h1>meu nome é: {{ nome }}</h1>
+</template>
+<script>
+export default {
+  name: "LifeCycle",
+  data() {
+    return {
+      nome: "ainda não sei",
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.nome = "Jorginho gameplay";
+    }, 2000);
+  },
+  mounted() {
+    setTimeout(() => {
+      this.nome = "Serjão gameplay";
+    }, 5000);
+  },
+};
+</script>
