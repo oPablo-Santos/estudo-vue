39d7ecaf94906efff9b22e88d63b65b071f08f5f
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-01-31 21:20:21 -0300

    Revisão vue estudo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> Hello world </h1>
+  <LifeCycle/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LifeCycle from './components/LifeCycle.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LifeCycle
   }
+ 
 }
 </script>
 
