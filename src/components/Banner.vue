a07c285b1e081a815dcadd25dd0dc9053c6179d8
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-02-02 20:52:35 -0300

    update make your burger

---
+++ src/components/Banner.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="main">
+    <h1>Make Your Burger</h1>
+  </div>
+</template>
+<script>
+export default {
+    name: "Banner"
+};
+</script>
+<style scoped>
+#main {
+  background-image: url("/public/img/burger.jpg");
+  background-position: 0 -80%;
+  background-size: cover;
+  height: 250px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+#main h1{
+    color: #fff;
+    text-align: center;
+    font-size: 60px;
+    background-color: #222;
+}
+</style>
